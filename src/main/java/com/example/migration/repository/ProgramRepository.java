b0e829086a20f67d4c69d6ff4dde3ce45f847a15
ksb <ksb>
2018-05-09 14:12:34 +0900

    test

--- src/main/java/com/example/migration/repository/ProgramRepository.java
+++ src/main/java/com/example/migration/repository/ProgramRepository.java
@@ -6,9 +6,11 @@ import com.example.migration.entity.ProgramCategory;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Set;
 
 @Repository
 public interface ProgramRepository extends CrudRepository<Program, String> {
@@ -31,5 +33,8 @@ public interface ProgramRepository extends CrudRepository<Program, String> {
 
     @Query(name = "selectProgramAndCategoryWrapperList")
     List<Object[]> selectProgramAndCategoryWrapperList();
+
+    @Query("SELECT p.id FROM Program p WHERE p.id IN :idList")
+    Set<String> findIdSetByIdList(@Param("idList") Set<String> idList);
 
 }
